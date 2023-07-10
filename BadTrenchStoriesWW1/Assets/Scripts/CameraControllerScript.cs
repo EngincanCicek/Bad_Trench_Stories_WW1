bcfffbb66ae5365f30ce9cdc469c32eb78f35a0c
Engincan Çiçek <mstrengincan22tr@gmail.com>
2023-07-11 00:03:49 +0300

    Isometric Strategy Game Camera X & Z Axis Update
    Now rotation is not functional.

--- BadTrenchStoriesWW1/Assets/Scripts/CameraControllerScript.cs
+++ BadTrenchStoriesWW1/Assets/Scripts/CameraControllerScript.cs
@@ -1,59 +1,89 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraControllerScript : MonoBehaviour
 {
+    public float speed;
+    public float zoomSpeed;
+    public float rotationSpeed = 0.1f;
 
-    public float normalSpeed;
-    public float fastSpeed;
-    public float movementSpeed;
-    public float movementTime;
-    public Vector3 newPosition;
+    private float maxHeight = 150;
+    private float minHeight = 20;
+
+    Vector2 p1;
+    Vector2 p2;
 
     // Start is called before the first frame update
     void Start()
     {
-        newPosition= transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandleCameraMovementInput();
-    }
-
-    private void HandleCameraMovementInput()
-    {
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            movementSpeed = fastSpeed;
+            speed = 0.005f;
+            zoomSpeed = 12;
         }
         else
         {
-            movementSpeed = normalSpeed;
+            speed = 0.002f;
+            zoomSpeed = 6;
+
         }
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        float hsp = transform.position.y * speed * Input.GetAxis("Horizontal");
+        float vsp = transform.position.y * speed * Input.GetAxis("Vertical");
+        float scrollSpeed = Mathf.Log(transform.position.y) * zoomSpeed * Input.GetAxis("Mouse ScrollWheel");
+
+
+        if ((transform.position.y >= maxHeight) && scrollSpeed > 0)
         {
-            newPosition += (transform.forward * movementSpeed);
+            scrollSpeed = 0;
         }
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        else if ((transform.position.y <= minHeight) && scrollSpeed < 0)
         {
-            newPosition += (transform.forward * -movementSpeed);
-        }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            newPosition += (transform.right * -movementSpeed);
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            newPosition += (transform.right * movementSpeed);
+            scrollSpeed = 0;
         }
 
-        transform.position = Vector3.Lerp(transform.position, newPosition, movementSpeed * Time.deltaTime);
 
+        Vector3 verticleMove = new Vector3(0, scrollSpeed, 0);
+        Vector3 lateralMove = hsp * transform.right;
+        Vector3 forwardMove = transform.forward;
+
+        forwardMove.y = 0;
+        forwardMove.Normalize();
+        forwardMove *= vsp;
+
+        Vector3 move = verticleMove + forwardMove + lateralMove;
+        transform.position += move;
+
+        getCameraRotation();
     }
 
-}
+    void getCameraRotation()
+    {
+        if (Input.GetMouseButtonDown(2)) // clicked!
+        {
+            Debug.Log("basýldý");
+            p1 = Input.mousePosition;
+        }
 
+        if (Input.GetMouseButton(2)) // keep going
+        {
+            Debug.Log("basýldý2");
+
+            p2 = Input.mousePosition;
+            float dx = (p2 - p1).x * rotationSpeed;
+            float dy = (p2 - p1).x * rotationSpeed;
+
+            transform.rotation *= Quaternion.Euler(new Vector3(0, dx, 0));
+        }
+
+
+    }
+}
