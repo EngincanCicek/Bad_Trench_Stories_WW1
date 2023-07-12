1748614da42bea856d40acd4731f03b93fa6c2c9
Engincan Çiçek <mstrengincan22tr@gmail.com>
2023-07-12 04:08:16 +0300

    Selection Added But Not Functional

---
+++ BadTrenchStoriesWW1/Assets/Scripts/Selections/selection_component.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selection_component : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+
+    }
+
+    private void OnDestroy()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+
+    }
+}
